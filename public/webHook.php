1dc52527b6fd21598c4a4b4c81f493f3d94a342c
Hui-Curry <2433472340@qq.com>
2020-12-25 18:40:13 +0800

    hui

--- public/webHook.php
+++ public/webHook.php
@@ -4,10 +4,11 @@ $requestBody = file_get_contents("php://input");    //接收数据
 if (empty($requestBody)) {              //判断数据是不是空
     die('send fail');
 }
+$webPath = '/www/wwwroot/zhibo.yangyonghui.com';
 $content = json_decode($requestBody, true);     //数据转换
+file_put_contents($webPath.'/git_web_hook.log', $requestBody, FILE_APPEND);//将每次拉取信息追加写入到日志里
 //若是主分支且提交数大于0
 if ($content['ref']=='refs/heads/main') {
-    $webPath = '/www/wwwroot/zhibo.yangyonghui.com';
 
     //或将命令加入 shell里，看个人需求 git reset --hard origin/master && git clean -f
     $res = shell_exec('cd '.$webPath.' && echo `sudo git pull` >> '.$webPath.'/git_web_hook.log');//PHP函数执行git命令
