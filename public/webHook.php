f639616913019510c6392088a10ce94a4abba420
Hui-Curry <2433472340@qq.com>
2020-12-25 18:44:37 +0800

    hui

--- public/webHook.php
+++ public/webHook.php
@@ -10,7 +10,7 @@ file_put_contents($webPath.'/git_web_hook.log', json_encode($content), FILE_APPE
 //若是主分支且提交数大于0
 if ($content['ref']=='refs/heads/main') {
 
-    //或将命令加入 shell里，看个人需求 ss git reset --hard origin/master && git clean -f
+    //或将命令加入 shell里，看个人需求 git reset --hard origin/master && git clean -f
     $res = shell_exec('cd '.$webPath.' && echo `sudo git pull` >> '.$webPath.'/git_web_hook.log');//PHP函数执行git命令
     $res_log = '-------------------------'.PHP_EOL;
     $res_log .= ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push'.$res;
